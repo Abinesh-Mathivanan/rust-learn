9a94616ec5587ceef50f49712c8f70d09fa3dc80
Abinesh-Mathivanan <abineshmathivanan31@gmail.com>
2025-01-17 21:49:24 +0530

    rust exercise

---
+++ src/bin/rust-exercise.rs
@@ -0,0 +1,36 @@
+use std::io;
+
+// fn main(){
+//     let mut input = String::new();
+//     io::stdin()
+//         .read_line(&mut input)
+//         .expect("expected string"); 
+//     let input_vec: Vec<&str> = input.trim().split_whitespace().collect(); 
+//     print!("{:?}", input_vec[0]);
+// }
+
+/* struct Rectangle {
+    height: i32,
+    width: i32,
+} 
+
+impl Rectangle {
+    fn new(height: i32, width: i32) -> Self {
+        return Rectangle {height, width} 
+    }
+
+    fn area(&self) -> i32 {
+        return self.height * self.width;
+    }
+
+    fn isflexible(&self, total: i32) -> bool{
+        return self.area() >= total;
+    }
+}
+
+fn main(){
+    let rect = Rectangle::new(10, 30);
+    println!("{}", rect.area());
+    println!("{}", rect.isflexible(100));
+} */ 
+
